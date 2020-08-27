69ca1a8a1273505c2b3e6b9d76a415aec3ecfd57
Kaan Taze <bkaantaze@gmail.com>
2020-08-27 14:18:21 +0300

    Added project

---
+++ Security/AppIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Security
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppIdentityUser, AppIdentityRole, string>
+    {
+        public AppIdentityDbContext
+        (DbContextOptions<AppIdentityDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebApplication1.Models;
+using WebApplication1.Security;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+
+        private IConfiguration config = null;
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(
+            options => options.UseSqlServer
+            (this.config.GetConnectionString("AppDb")));
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(this.config.GetConnectionString("AppDb")));
+
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.LoginPath = "/Security/SignIn";
+                opt.AccessDeniedPath = "/Security/AccessDenied";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
+            });
+        }
+    }
+   
+}
